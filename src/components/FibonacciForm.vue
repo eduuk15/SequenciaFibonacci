7b34124d6d09badbe6273aeceee2e04dc885df16
Eduardo <eduardo@conectra.com.br>
2022-09-29 18:47:51 -0300

    style: teste css iphone

--- src/components/FibonacciForm.vue
+++ src/components/FibonacciForm.vue
@@ -30,15 +30,15 @@
         <input
           type="number"
           id="search"
-          class="p-4 pl-10 w-80 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="text-center p-4 pl-10 w-80 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           v-model="termo"
           placeholder="Insira o termo que deseja buscar"
-          required
+          requireds
         >
 
         <button
           type="submit"
-          class="text-white absolute left-24 top-24 px-6 bg-violetImage hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white absolute inset-x-20 top-20 px-6 bg-violetImage hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Pesquisar
         </button>
