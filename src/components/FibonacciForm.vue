85916082778426370f635440e368500649e6cfd3
Eduardo <eduardo@conectra.com.br>
2022-09-29 13:31:24 -0300

    feat: volta codigo

--- src/components/FibonacciForm.vue
+++ src/components/FibonacciForm.vue
@@ -103,4 +103,9 @@ export default {
 </script>
 
 <style>
+  input[type=number]::-webkit-inner-spin-button,
+  input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 </style>
